0c04ac3e3978e0ea5010d9a6d4c94ccdd237a0b8
Xander-97 <60773912+Xander-97@users.noreply.github.com>
2021-04-05 16:37:25 +0800

    first commit 2021-4-5 16:37:05

---
+++ VBApp1/VBApp1/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Button1.Text = "这是按钮"
+        Button1.Height = Button1.Height + 10
+        Button1.Width = Button1.Width + 10
+        'Print(Button1.Width)
+        'Print(Button1.Height)
+    End Sub
+End Class
